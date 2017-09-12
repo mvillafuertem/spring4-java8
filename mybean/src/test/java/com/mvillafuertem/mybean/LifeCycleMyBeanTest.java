f501080f5f48a1d14f7bb25c2e5a0c4f0e98f061
Miguel <mivimur@hotmail.com>
2017-09-12 12:12:25 +0200

    Add test of my beans

--- mybean/src/test/java/com/mvillafuertem/mybean/LifeCycleMyBeanTest.java
+++ mybean/src/test/java/com/mvillafuertem/mybean/LifeCycleMyBeanTest.java
@@ -2,19 +2,36 @@ package com.mvillafuertem.mybean;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
 
 /**
  * Created by mvillafuertem on 9/11/17.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/myApplicationContextTest.xml")
 public class LifeCycleMyBeanTest {
+
+    @Autowired
+    private LifeCycleMyBean lifeCycleMyBean;
+
     @Before
     public void setUp() throws Exception {
     }
 
     @Test
     public void afterPropertiesSet() throws Exception {
+    }
+
+    @Test
+    public void main() {
+
+        assertThat(this.lifeCycleMyBean).isNotNull();
     }
 
 }
--- mybean/src/test/java/com/mvillafuertem/mybean/MyBeanTest.java
+++ mybean/src/test/java/com/mvillafuertem/mybean/MyBeanTest.java
@@ -2,13 +2,24 @@ package com.mvillafuertem.mybean;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.*;
 
 /**
  * Created by mvillafuertem on 9/11/17.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/myApplicationContextTest.xml")
 public class MyBeanTest {
+
+    @Autowired
+    private MyBean myBean;
+
     @Before
     public void setUp() throws Exception {
     }
@@ -19,6 +30,14 @@ public class MyBeanTest {
 
     @Test
     public void setMessage() throws Exception {
+    }
+
+    @Test
+    public void main() {
+
+        assertThat(this.myBean).isNotNull();
+        assertThat(this.myBean.getMessage()).isNotEmpty();
+        assertThat(this.myBean.getMessage()).isEqualTo("Mensaje desde myApplicationContext");
     }
 
 }
